0ab2301a8cb62fd251d21732f88c922ed0db107e
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-09-18 22:04:47 +0530

    app setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import app from "./app.js";
 import connectDb from "./db/index.js";
-const port = 4000
+import 'dotenv/config'
+const port = process.env.PORT || 4000
 connectDb().then(()=>{
     app.listen(port,()=>{
-        console.log("Active at port ",port)
+        console.log("Active at port",port)
     })
 }).catch((err)=>{
     console.log("Some error occured ",err)
