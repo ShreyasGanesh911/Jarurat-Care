ce76aff42136b47942b1e97967f62277aea48fbf
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-09-18 22:04:41 +0530

    app setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from 'express'
 import APIError from './utils/APIError.js'
+import serviceRouter from './routes/service.route.js'
+
 const app = express()
+app.use(express.json())
+app.use("/service",serviceRouter)
 app.use(APIError)
 export default app
