20732fc3648c5ca686bfef146e24deac3afd9be5
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-09-18 22:05:07 +0530

    db connection

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async()=>{
    try{
-    await mongoose.connect("mongodb://localhost:27017/JaruratCare")
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("Connected to db")
    }catch(err){
     console.log("Error in connecting to DB ",err)
